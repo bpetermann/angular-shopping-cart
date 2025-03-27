4e6844f19a53d3ac6fc38298638d6c61c56c6bf1
bpetermann <benjamin.petermann@gmx.at>
2025-03-27 21:49:56 +0100

    feat: add hero component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './organisms/header/header.component';
+import { HeroComponent } from './organisms/hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
