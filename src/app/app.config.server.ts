524a1ad72caed96d5ae2c90521aba203af2e078e
bpetermann <benjamin.petermann@gmx.at>
2025-04-21 20:56:25 +0200

    chore: setup http client

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,4 +1,5 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
@@ -7,8 +8,9 @@ import { serverRoutes } from './app.routes.server';
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRouting(serverRoutes)
-  ]
+    provideServerRouting(serverRoutes),
+    provideHttpClient(),
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
