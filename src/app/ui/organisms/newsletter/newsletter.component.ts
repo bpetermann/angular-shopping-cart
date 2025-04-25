a7aaf18f78370641b5f747efff2dba16e9e6c1ea
bpetermann <benjamin.petermann@gmx.at>
2025-04-25 14:57:05 +0200

    style: update newsletter

--- src/app/ui/organisms/newsletter/newsletter.component.ts
+++ src/app/ui/organisms/newsletter/newsletter.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, signal } from '@angular/core';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
+import { MailSVGComponent, SuccessSVGCommponent } from '../../atoms/svg';
 
 @Component({
   selector: 'app-newsletter',
-  imports: [FormsModule],
+  imports: [FormsModule, MailSVGComponent, SuccessSVGCommponent],
   templateUrl: './newsletter.component.html',
   styleUrl: './newsletter.component.scss',
 })
 export class NewsletterComponent {
-  onSubmit({ form }: NgForm) {
-    console.log('Email: ', form.controls['email'].value);
-    console.log('Fashion: ', form.controls['fashion'].value);
+  newsletterSubsription = signal<boolean>(false);
+
+  isInvalid(email: NgModel) {
+    return email.touched && email.dirty && email.invalid && email.value;
+  }
+
+  onSubmit(_form: NgForm) {
+    this.newsletterSubsription.set(true);
   }
 }
