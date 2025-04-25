b89e72bbf7f63a27bf130d63963e79020dad8cf2
bpetermann <benjamin.petermann@gmx.at>
2025-04-25 21:17:52 +0200

    feat: add reactive newsletter form

--- src/app/ui/organisms/newsletter/newsletter.component.ts
+++ src/app/ui/organisms/newsletter/newsletter.component.ts
@@ -1,21 +1,53 @@
 import { Component, signal } from '@angular/core';
-import { FormsModule, NgForm, NgModel } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MailSVGComponent, SuccessSVGCommponent } from '../../atoms/svg';
 
 @Component({
   selector: 'app-newsletter',
-  imports: [FormsModule, MailSVGComponent, SuccessSVGCommponent],
+  imports: [
+    FormsModule,
+    MailSVGComponent,
+    SuccessSVGCommponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './newsletter.component.html',
   styleUrl: './newsletter.component.scss',
 })
 export class NewsletterComponent {
-  newsletterSubsription = signal<boolean>(false);
+  form = new FormGroup({
+    email: new FormControl(null, {
+      validators: [Validators.required, Validators.email],
+    }),
+    fashion: new FormControl('wFashion'),
+  });
 
-  isInvalid(email: NgModel) {
-    return email.touched && email.dirty && email.invalid && email.value;
+  isSubscribed = signal<boolean>(false);
+
+  get emailIsInvalid() {
+    const { email } = this.form.controls;
+    return email.touched && email.dirty && email.invalid;
   }
 
-  onSubmit(_form: NgForm) {
-    this.newsletterSubsription.set(true);
+  get emailValue() {
+    return this.form.controls.email.value;
+  }
+
+  reset() {
+    this.form.controls.email.markAsUntouched();
+  }
+
+  onSubmit() {
+    if (!this.emailValue) {
+      this.form.controls.email.markAsDirty();
+      return;
+    }
+
+    this.isSubscribed.set(true);
   }
 }
