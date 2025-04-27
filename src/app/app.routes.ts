a6b5852942f9aa609e93e1fca1f037c0852ab0f1
bpetermann <benjamin.petermann@gmx.at>
2025-04-27 11:34:04 +0200

    chore: setup app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './ui/templates/home/home.component';
+import { ProductComponent } from './ui/templates/product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'product',
+    component: ProductComponent,
+  },
+];
