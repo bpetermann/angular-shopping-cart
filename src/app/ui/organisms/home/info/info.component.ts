1c881939ff58a145a9f3bd3bd6f08a845130eb78
bpetermann <benjamin.petermann@gmx.at>
2025-05-02 11:46:30 +0200

    feat: add info component

---
+++ src/app/ui/organisms/home/info/info.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  HelpSVGComponent,
+  RedeemSVGComponent,
+  TruckSVGComponent,
+} from '@atoms/svg';
+import { FaqComponent } from '@molecules/shared/faq/faq.component';
+
+@Component({
+  selector: 'app-info',
+  imports: [FaqComponent, CommonModule],
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.scss',
+})
+export class InfoComponent {
+  faqs = [
+    {
+      question: 'Help & Contact',
+      answer:
+        'When you complete your order, we will show you in what timeframe your package is expected to arrive',
+      svg: HelpSVGComponent,
+    },
+    {
+      question: 'Gift cards',
+      answer:
+        'You can redeem your promotional voucher in the last step of the order process.',
+      svg: RedeemSVGComponent,
+    },
+    {
+      question: 'Shipping',
+      answer:
+        'If you are shipping internationally, there are several delivery speed options available during the checkout process, usually standard, express and priority delivery.',
+      svg: TruckSVGComponent,
+    },
+  ];
+}
--- src/app/ui/templates/home/home.component.ts
+++ src/app/ui/templates/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { InfoComponent } from '@organisms/home/info/info.component';
 import { HeroComponent } from '../../organisms/home/hero/hero.component';
 import { NewsletterComponent } from '../../organisms/home/newsletter/newsletter.component';
 import { ProductsComponent } from '../../organisms/home/products/products.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, ProductsComponent, NewsletterComponent],
+  imports: [
+    HeroComponent,
+    ProductsComponent,
+    NewsletterComponent,
+    InfoComponent,
+  ],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
