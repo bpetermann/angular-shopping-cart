b84448f5e3b8694ca562461fc2cfbbec74dbd9f4
bpetermann <benjamin.petermann@gmx.at>
2025-05-15 21:28:29 +0200

    style: add account page

--- src/app/ui/templates/account/account.component.ts
+++ src/app/ui/templates/account/account.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { PrimaryButtonComponent } from '@atoms/primary-button/primary-button.component';
 
 @Component({
   selector: 'app-account',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   templateUrl: './account.component.html',
-  styleUrl: './account.component.scss'
+  styleUrl: './account.component.scss',
 })
 export class AccountComponent {
+  view = signal<'login' | 'register'>('login');
 
+  isLoginView = computed(() => this.view() === 'login');
+
+  isRegisterView = computed(() => this.view() === 'register');
+
+  onRegister() {
+    if (this.isRegisterView()) console.log('Register Attempt');
+    else this.view.set('register');
+  }
+
+  onLogin() {
+    if (this.isLoginView()) console.log('Login Attempt');
+    else this.view.set('login');
+  }
 }
