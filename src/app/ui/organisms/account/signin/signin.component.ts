5a6533ac58d90b3c852c1510c71bfc4ba58f48c8
bpetermann <benjamin.petermann@gmx.at>
2025-05-16 21:50:22 +0200

    feat: add signin form

--- src/app/ui/organisms/account/signin/signin.component.ts
+++ src/app/ui/organisms/account/signin/signin.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { PrimaryButtonComponent } from '@atoms/primary-button/primary-button.component';
 
 @Component({
   selector: 'app-signin',
-  imports: [PrimaryButtonComponent],
+  imports: [FormsModule, PrimaryButtonComponent, ReactiveFormsModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss',
 })
-export class SigninComponent {}
+export class SigninComponent {
+  form = new FormGroup({
+    email: new FormControl(null, {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl(null, {
+      validators: [Validators.required],
+    }),
+  });
+
+  passwordInvalid = signal<boolean>(false);
+
+  get emailIsInvalid() {
+    const { email } = this.form.controls;
+    return email.touched && email.dirty && email.invalid;
+  }
+
+  resetEmailError() {
+    this.form.controls.email.markAsUntouched();
+  }
+
+  resetPasswordError() {
+    this.passwordInvalid.set(false);
+  }
+
+  onSubmit() {
+    const { email, password } = this.form.controls;
+
+    if (!email.value) {
+      email.markAsDirty();
+      email.markAsTouched();
+      return;
+    }
+
+    if (!password.value) {
+      this.passwordInvalid.set(true);
+      return;
+    }
+
+    console.log(email.value);
+    console.log(password.value);
+  }
+}
